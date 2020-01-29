4eb8960900d05fbbcbf762fc3d485c9163fef9ca
pedro <pedro.phdois@gmail.com>
2020-01-29 11:41:55 -0300

    811c937

---
+++ precache-manifest.376bba9bddffda1ff47f33d426b0b66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c25d19217399b5ccf2e3d8ab635035",
+    "url": "/joketem-react/index.html"
+  },
+  {
+    "revision": "4fd75569a37117d80d9b",
+    "url": "/joketem-react/static/css/main.89fac48d.chunk.css"
+  },
+  {
+    "revision": "07d579e5a1b64238dac0",
+    "url": "/joketem-react/static/js/2.6c37fffd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/joketem-react/static/js/2.6c37fffd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fd75569a37117d80d9b",
+    "url": "/joketem-react/static/js/main.0092e4b7.chunk.js"
+  },
+  {
+    "revision": "d71208300e0a0bc81139",
+    "url": "/joketem-react/static/js/runtime-main.e146faa5.js"
+  },
+  {
+    "revision": "84e6637253a01894b47ab8d4b9433074",
+    "url": "/joketem-react/static/media/logo.84e66372.svg"
+  }
+]);
